e8023a830b82666afb0a0d265cfcb5eb72c34071
Gilad Novik <gilad@novik.ca>
2017-10-13 02:07:39 +0300

    react router

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,18 @@
 require('normalize.css');
 require('./css/style.less');
 import React, {Component} from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+const App = ()=><h1>Hello, world</h1>;
 
 export default class extends Component {
     render(){
         return (
-            <h1>Hello, world</h1>
+            <BrowserRouter>
+                <div>
+                    <Route path='/' exact component={App} / >
+                </div>
+            </BrowserRouter>
         );
     }
 }
