2d6bde717589364b64a10fd121464bc7bb4e6740
Gilad Novik <gilad@novik.ca>
2017-10-13 06:32:57 +0300

    separate router from routes

--- src/app.jsx
+++ src/app.jsx
@@ -1,18 +1,16 @@
 require('normalize.css');
 require('./css/style.less');
 import React, {Component} from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 
 const App = ()=><h1>Hello, world</h1>;
 
 export default class extends Component {
     render(){
         return (
-            <BrowserRouter>
-                <div>
-                    <Route path='/' exact component={App} / >
-                </div>
-            </BrowserRouter>
+            <Route>
+                <Route exact path='/' component={App} />
+            </Route>
         );
     }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,16 @@
 import {AppContainer} from 'react-hot-loader';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
 import App from './app.jsx';
 
-const render = Component=>ReactDOM.render(<AppContainer><Component /></AppContainer>, document.getElementById('root'));
+const render = ()=>ReactDOM.render(
+    <AppContainer>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </AppContainer>, document.getElementById('root'));
 
-render(App);
+render();
 if (module.hot)
-    module.hot.accept('./app.jsx', ()=>render(App));
+    module.hot.accept('./app.jsx', render);
