21a1ae2e76d9c463d0bb6a35349fca0ab1825cca
Gilad Novik <gilad@novik.ca>
2017-11-27 03:44:50 +0200

    use PureComponent

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,11 @@
 require('normalize.css');
 require('./css/style.less');
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 import {Route} from 'react-router-dom';
 
 const App = ()=><h1>Hello, world</h1>;
 
-export default class extends Component {
+export default class extends PureComponent {
     render(){
         return (
             <Route>
