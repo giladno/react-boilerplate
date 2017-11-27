8832f2e51b48d66f5c648f8b08ea5c524225e801
Gilad Novik <gilad@novik.ca>
2017-11-27 03:30:52 +0200

    enable CORS for dev

--- server.js
+++ server.js
@@ -29,6 +29,9 @@ if (__DEV__)
         res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
         res.header('Expires', '-1');
         res.header('Pragma', 'no-cache');
+        res.header('Access-Control-Allow-Origin', req.headers.origin||'*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        res.header('Access-Control-Allow-Credentials', 'true');
         req.body && Object.keys(req.body).length && console.log(req.body);
         next();
     });
