e4d806b0f26e077224a8360768fe0bbb96a8c87c
Gilad Novik <gilad@novik.ca>
2018-06-09 04:02:36 +0300

    hmr fix

--- src/app.jsx
+++ src/app.jsx
@@ -2,13 +2,13 @@ require('normalize.css');
 require('./css/style.less');
 import {Route} from 'react-router-dom';
 
-const App = () => <h1>Hello, world</h1>;
+const Main = () => <h1>Hello, world</h1>;
 
-export default class extends React.Component {
+export class App extends React.Component {
     render() {
         return (
             <Route>
-                <Route exact path="/" component={App} />
+                <Route exact path="/" component={Main} />
             </Route>
         );
     }
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
-import App from './app.jsx';
 
-const render = () =>
+const render = () => {
+    const {App} = require('./app.jsx');
     ReactDOM.render(
         <BrowserRouter>
             <App />
         </BrowserRouter>,
         document.getElementById('root')
     );
-
+};
 render();
 if (module.hot) module.hot.accept('./app.jsx', render);
